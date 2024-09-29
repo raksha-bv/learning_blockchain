4e9847f3dc91bab3307d2395a541a9c321a7b849
raksha-bv <rakshabv04@gmail.com>
2024-09-29 22:29:41 +0530

    foundry basics

---
+++ simpleStorage_foundry/src/SimpleStorage.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+contract SimpleStorage{
+    uint256 public myFavNum;
+    mapping(string=>uint256) public nameToNumber;
+
+    function store(uint256 _myFavNum) public virtual  {
+        myFavNum=_myFavNum;
+    }
+
+    struct Person{
+        uint256 favNum;
+        string name;
+    }
+
+    function retrieve() public view returns(uint256){
+        return myFavNum;
+    }
+
+    Person[] public listOfPpl;
+
+    function addPpl(uint256 _favNum, string memory _name) public{
+        listOfPpl.push(Person({favNum:_favNum, name:_name }));
+        nameToNumber[_name]=_favNum;
+    }
+
+
+}
